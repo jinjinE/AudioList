325776a8a8187a3196f2d1e690bff1aac3249947
every-sushi <nadia0515@naver.com>
2017-06-20 20:55:29 +0900

    #6 Move SeekBar

--- app/src/main/java/emirim0223/kr/hs/emirim/audiolist/MainActivity.java
+++ app/src/main/java/emirim0223/kr/hs/emirim/audiolist/MainActivity.java
@@ -1,6 +1,7 @@
 package emirim0223.kr.hs.emirim.audiolist;
 
 import android.media.MediaPlayer;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,14 +10,17 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
     ListView list;
     Button butPlay, butStop,butPause;
-    TextView textMusic;
-    ProgressBar progress;
+    TextView textMusic,TextTime;
     MediaPlayer musicPlayer;
+    SeekBar progress;
     String[] musics = {"boneafour","iwish","mrpotter","sandeul","staywithme","whoareyou"};
     int[] musicResIds = {R.raw.boneafour,R.raw.iwish,R.raw.mrpotter,R.raw.sandeul,R.raw.staywithme,R.raw.whoareyou};
     int selectedMusicId;
@@ -30,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
         butStop = (Button)findViewById(R.id.but_stop);
         butPause = (Button)findViewById(R.id.but_pause);
         textMusic = (TextView)findViewById(R.id.text_music);
-        progress = (ProgressBar)findViewById(R.id.progress_music);
+        TextTime = (TextView)findViewById(R.id.text_time);
+        progress = (SeekBar) findViewById(R.id.progress_music);
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice,musics);
         list.setAdapter(adapter);
         list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
@@ -44,14 +49,31 @@ public class MainActivity extends AppCompatActivity {
                 musicPlayer.stop();
                 selectedMusicId = musicResIds[i];
                 progress.setVisibility(View.INVISIBLE);
+                MainActivity.this.i=1;
+
             }
         });
         butPlay.setOnClickListener(new View.OnClickListener() {
+            SimpleDateFormat timeFormat = new SimpleDateFormat("mm:ss");
             @Override
             public void onClick(View view) {
                 musicPlayer = MediaPlayer.create(MainActivity.this,selectedMusicId);
                 musicPlayer.start();
                 progress.setVisibility(View.VISIBLE);
+                Thread musicThread = new Thread(){
+                    @Override
+                    public void run() {
+                        if(musicPlayer == null){
+                            return;
+                        }
+                        progress.setMax(musicPlayer.getDuration());
+                            progress.setProgress(musicPlayer.getCurrentPosition());
+                        TextTime.setText("진행시간 : "+timeFormat.format(musicPlayer.getCurrentPosition()));
+                        SystemClock.sleep(200);
+                    }
+                };
+                musicThread.start();
+
             }
         });
         butStop.setOnClickListener(new View.OnClickListener() {
